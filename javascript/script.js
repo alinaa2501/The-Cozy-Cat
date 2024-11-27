db7a837a219cbcc139088d60d7e79c01c1eacf30
Alina Pobiedynska <alinaa2501@gmail.com>
2024-11-27 15:37:12 +0100

    modals

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,28 @@
-
-
 const employees = document.querySelectorAll('.employe');
 
 employees.forEach(employee => {
-  employee.addEventListener('mouseover', function () {
+  employee.addEventListener('mouseover',  () => {
     employee.style.backgroundColor = 'rgb(159, 104, 231, 0.5)'; // Nieuwe achtergrondkleur
+    let text = employee.querySelectorAll('p'); // Selecteer alle <p> binnen deze employee
+    text.forEach(p => p.style.color = 'white');
+    let h3 = employee.querySelectorAll('h3');
+    h3.forEach(h => h.style.color = 'white');
   });
 
-  employee.addEventListener('mouseout', function () {
+  employee.addEventListener('mouseout',  () => {
     employee.style.backgroundColor = ''; // Reset naar oorspronkelijke kleur
+    const text = employee.querySelectorAll('p'); // Selecteer opnieuw de <p>-elementen
+    text.forEach(p => p.style.color = '')
+    let h3 = employee.querySelectorAll('h3');
+    h3.forEach(h => h.style.color = '');
   });
 });
+
+
+const buttonSend = document.querySelector('.btn-send')
+
+function showMessage(event) {
+  event.preventDefault()
+}
+
+buttonSend.addEventListener('click', showMessage)
